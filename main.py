cefb1448e0047951686f45228a895c5a0efbfff2
Etienne Posthumus <eposthumus@gmail.com>
2024-01-29 14:54:20 +0100

    Add some logging info

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ from helper import fetch_gpu_infos, fetch_process_info, get_host_name
 from storing import store_as_sqlite
 from typing import List
 
+import logging
+
+if os.environ.get("DEBUG") == "1":
+    logging.basicConfig(level=logging.DEBUG)
+
 
 def main(hosts: List[str]) -> None:
     for host_id in hosts.split(" "):
+        logging.debug(f"Fetching GPU data from {host_id}")
         gpu_infos = fetch_gpu_infos(host_id)
+        logging.debug(f"Fetching process data from {host_id}")
         process_infos = [
             fetch_process_info(host_id, pid=gpu_info.pid) for gpu_info in gpu_infos
         ]
@@ -21,6 +28,7 @@ if __name__ == "__main__":
     hosts = os.environ.get("HOSTS")
     main(hosts)
     while True:
+        logging.debug("Sleeping for 25 seconds before next run.")
         time.sleep(25)
         main(hosts)
         # In a cronjob, the minimum interval is 1 minute, but we want to run this job every 30 seconds.
