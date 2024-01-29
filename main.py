f0fb798c3cce4b4528e595530bb0782c877b4151
Etienne Posthumus <eposthumus@gmail.com>
2024-01-29 14:44:35 +0100

    add repetition for cronjob sub-minute interval

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
-import os
+import os, time
 
 from data_types import GpuInfo, ProcessInfo
 from helper import fetch_gpu_infos, fetch_process_info, get_host_name
 from storing import store_as_sqlite
+from typing import List
 
 
-if __name__ == "__main__":
-    # We expect a list of hostnames that can be SSH'ed into from the account running this script.
-    # for convenience, sconfigure shortcut names in the .ssh/config file.
-    hosts = os.environ.get("HOSTS")
+def main(hosts: List[str]) -> None:
     for host_id in hosts.split(" "):
         gpu_infos = fetch_gpu_infos(host_id)
         process_infos = [
             fetch_process_info(host_id, pid=gpu_info.pid) for gpu_info in gpu_infos
         ]
         store_as_sqlite(gpu_infos, process_infos, host_id=host_id, target_dir="tmp")
+
+
+if __name__ == "__main__":
+    # We expect a list of hostnames that can be SSH'ed into from the account running this script.
+    # for convenience, sconfigure shortcut names in the .ssh/config file.
+    hosts = os.environ.get("HOSTS")
+    main(hosts)
+    while True:
+        time.sleep(25)
+        main(hosts)
+        # In a cronjob, the minimum interval is 1 minute, but we want to run this job every 30 seconds.
+        # That's why we use this trick to get sub-minute intervals.
